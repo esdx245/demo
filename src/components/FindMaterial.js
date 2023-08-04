76f99529fd806ffd5540a5c79ad4a7d380f75a1d
raipen <raipendalk@gmail.com>
2023-08-04 16:18:31 +0900

    design:update

--- src/components/FindMaterial.js
+++ src/components/FindMaterial.js
@@ -1,9 +1,27 @@
 import { useRef,useState,useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
+import {ReactComponent as AddIcon} from "../add_circle.svg";
+
+const Search = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top:10px;
+    > button {
+        height: 50px;
+        margin: 6px 0 16px 0;
+        padding-top: 0;
+        padding-bottom: 0;
+    }
+    > input:nth-child(2) {
+        flex: 1;
+    }
+`;
 
 const Input = styled.input`
-    width: 100%;
     height: 100%;
     border: 1px solid #ccc;
     border-radius: 4px;
@@ -35,12 +53,51 @@ const ContentsWrapper = styled.div`
   background-color: #fff;
   margin: 5px auto;
   display: flex;
-  justify-content: center;
+  justify-content: start;
+  gap: 10px;
   align-items: center;
   width: 100%;
   border: 1px solid #ccc;
   border-radius: 10px;
   padding: 0 10px;
+  position: relative;
+  cursor: pointer;
+  >div{
+    border-left: 1px solid #ccc;
+    padding-left: 10px;
+    display: flex;
+    flex-direction: column;
+    >div{
+        color: #888;
+        font-size: 0.8rem;
+        >label{
+            margin:0;
+        }
+    }
+  }
+  >article{
+    position: absolute;
+    display:none;
+
+  }
+  &:hover{
+    >article{
+        display:flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #fff;
+        opacity: 0.7;
+        border-radius: 10px;
+        padding: 10px;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        right: 0;
+        z-index: 1;
+        text-align: left;
+    }
+  }
+}
 `;
 
 export default function FindMaterial({addFunction}) {
@@ -67,34 +124,31 @@ export default function FindMaterial({addFunction}) {
 
     return (
         <form>
-            <Input type="text" id="id" maxlength="20" placeholder="브랜드 이름" ref={brandRef}></Input>
-            <Input type="text" id="id" maxlength="20" placeholder="재료 이름" ref={searchRef}></Input>
-            <button type="submit" onClick={onSubmit}>검색</button>
+            <Search>
+                <Input type="text" id="id" maxlength="20" placeholder="브랜드 이름" ref={brandRef}></Input>
+                <Input type="text" id="id" maxlength="20" placeholder="재료 이름" ref={searchRef}></Input>
+                <button type="submit" onClick={onSubmit} className="btnplus">검색</button>
+            </Search>
             <ListWrapper>
                 {
                     material
                         .map((item, index) => {
                             return (
                                 <ContentsWrapper key={item.id} onClick={() => addFunction(item)}>
+                                    <article>
+                                        <AddIcon width="100px" height="80%"/>
+                                    </article>
                                     <Image src={"https://www.cntmart.com"+item.image}/>
                                     <div>
                                         <span>{item.brand}</span>
                                         <span>{item.name}</span>
                                         <div>
-                                            <label htmlFor="volume">용량</label>
-                                            <span>{item.volume+item.unit}</span>
-                                        </div>
-                                        <div>
-                                            <label htmlFor="count">개수</label>
-                                            <span>{item.count}개</span>
-                                        </div>
-                                        <div>
                                             <label htmlFor="price">가격</label>
-                                            <span>{item.price.toLocaleString()}원</span>
+                                            <span> {item.price.toLocaleString()}원</span>
                                         </div>
                                         <div>
                                             <label htmlFor="total">1{item.volume?item.unit:"개"}당 가격</label>
-                                            <span>{Math.floor((item.volume?item.price/(item.volume*item.count):item.price/item.count)*100)/100}</span>
+                                            <span> {Math.floor((item.volume?item.price/(item.volume*item.count):item.price/item.count)*100)/100}</span>
                                         </div>
                                     </div>
                                 </ContentsWrapper>
